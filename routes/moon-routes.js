4533dd8499b58d202cb73cd5f139bd9b800a5b5f
jkarlin929 <osida11572@gmail.com>
2018-01-05 17:10:57 -0500

    created routes directory and file

---
+++ routes/moon-routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const moonController = require('..controllers/moon-controller');
+const moonRouter = express.Router();
+
+moonRouter.get('/', moonController.index);
+
+
+
+
+module.exports = moonRouter;
