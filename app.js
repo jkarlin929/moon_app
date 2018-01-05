dc9ee0cdcaec4e900f63519897b1c91786a6eeb9
jkarlin929 <osida11572@gmail.com>
2018-01-05 17:02:58 -0500

    added router variable

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-
 const PORT = process.env.PORT || 3000;
+const moonRouter = require('./routes/moon-routes');
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello Friend');
@@ -12,6 +13,8 @@ app.get('/', (req, res) => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use('/moon', moonRouter);
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
