1f79fe6deb3714614cce39c547d07e21f908c481
jkarlin929 <osida11572@gmail.com>
2018-01-06 21:30:59 -0500

    added show model

--- models/moon.js
+++ models/moon.js
@@ -6,4 +6,8 @@ Moon.findAll = () => {
   return db.query('SELECT * FROM moonPhase');
 };
 
+Moon.findById = id => {
+  return db.oneOrNone(`SELECT * FROM moonPhase WHERE id = $1`, [id]);
+};
+
 module.exports = Moon;
