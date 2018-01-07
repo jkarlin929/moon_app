1fcdff56cc2138f0788800c3f7395e77fd30bad8
jkarlin929 <osida11572@gmail.com>
2018-01-06 22:30:05 -0500

    added models

--- models/moon.js
+++ models/moon.js
@@ -6,4 +6,19 @@ Moon.findAll = () => {
   return db.query('SELECT * FROM moonPhase');
 };
 
+Moon.findById = id => {
+  return db.query(`SELECT * FROM moonPhase WHERE id = $1`, [id]);
+};
+
+Moon.update = (moon, id) => {
+  return db.none(
+      `UPDATE moonPhase SET
+      date = $1,
+      imageurl = $2
+      WHERE id = $3
+      `,
+      [moon.date, moon.imageurl, id]
+    );
+};
+
 module.exports = Moon;
