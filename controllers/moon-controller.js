255aef10a6a9227beb581a4675dd40cd11bd21f3
jkarlin929 <osida11572@gmail.com>
2018-01-07 16:25:54 -0500

    added destory to controllers

--- controllers/moon-controller.js
+++ controllers/moon-controller.js
@@ -67,5 +67,15 @@ moonController.create = (req, res) => {
   });
 };
 
+moonController.destroy = (req, res) => {
+  Moon.destroy(req.params.id)
+  .then(() => {
+    res.redirect('/moon')
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+}
+
 
 module.exports = moonController;
