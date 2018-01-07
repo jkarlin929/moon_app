bcb79ed73f4d5531454d763287bdae6f3f162265
jkarlin929 <osida11572@gmail.com>
2018-01-07 18:15:26 -0500

    change query to oneOrNone

--- models/moon.js
+++ models/moon.js
@@ -7,14 +7,15 @@ Moon.findAll = () => {
 };
 
 Moon.findById = id => {
-  return db.query(`SELECT * FROM moonphase WHERE id = $1`, [id]);
+  // console.log(db)
+  return db.oneOrNone(`SELECT * FROM moonphase WHERE id = $1`, [id]);
 };
 
 Moon.update = (moon, id) => {
   return db.none(
       `
       UPDATE moonphase SET
-      date = $1,
+      date = $1
       WHERE id = $2
       `,
       [moon.date, id]
