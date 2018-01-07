ea735391d1f84cd65cb00124ceff483b508268ca
jkarlin929 <osida11572@gmail.com>
2018-01-06 22:08:18 -0500

    added edit route

--- routes/moon-routes.js
+++ routes/moon-routes.js
@@ -4,7 +4,7 @@ const moonRouter = express.Router();
 
 moonRouter.get('/', moonController.index);
 moonRouter.get('/:id', moonController.show);
-
+moonRouter.get('/:id/edit', moonController.edit);
 
 
 module.exports = moonRouter;
