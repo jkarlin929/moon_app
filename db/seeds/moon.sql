9191201347c36a170226e7c5ac50967208cb30fb
jkarlin929 <osida11572@gmail.com>
2018-01-08 18:25:10 -0500

    added moon_time to db

--- db/seeds/moon.sql
+++ db/seeds/moon.sql
@@ -1,11 +1,13 @@
 /c moon_phase
 
-INSERT INTO moonphase (moon_date, imageUrl) VALUES
+INSERT INTO moonphase (moon_date, moon_time, imageUrl) VALUES
 (
   '09/29/1986',
+  '8:17',
   'https://img.purch.com/h/1000/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzAxOS8wOTEvb3JpZ2luYWwvanVseS1za3l3YXRjaGluZy1wb3J0bGFuZC5qcGc='
 ),
 (
   '06/11/1990',
+  '12:30',
   'https://www.donutbar.com/wp-content/uploads/2017/05/donut-bar-homers.png'
 );
