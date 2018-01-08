8a0bf748f63347395d48aebc6aef05f87cdee0fd
jkarlin929 <osida11572@gmail.com>
2018-01-08 10:16:16 -0500

    edited route order to render new.ejs page

--- routes/moon-routes.js
+++ routes/moon-routes.js
@@ -3,10 +3,11 @@ const moonController = require('../controllers/moon-controller');
 const moonRouter = express.Router();
 
 moonRouter.get('/', moonController.index);
+moonRouter.get('/new', moonController.new);
 moonRouter.get('/:id', moonController.show);
 moonRouter.get('/:id/edit', moonController.edit);
 moonRouter.put('/:id', moonController.update);
-moonRouter.get('/new', moonController.new);
+
 moonRouter.post('/', moonController.create);
 moonRouter.delete('/:id', moonController.destroy);
 
