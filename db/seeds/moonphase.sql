22da302ee39413812b488d96a4d5f49e926eb3de
jkarlin929 <osida11572@gmail.com>
2018-01-11 17:06:51 -0500

    added migrations and created moonphasedata table

---
+++ db/seeds/moonphase.sql
@@ -0,0 +1,2 @@
+INSERT INTO moonphasedata (moonrise, moonset, closestphase, closestphasedate, closestphasetime, fracillum, currentphase) VALUES
+('2:00 am', '5:03 pm', 'Last Quarter', 'September 25, 1986', '11:17 pm', '18%', 'waning crescent');
