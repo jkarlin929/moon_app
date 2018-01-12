17efa66ec9ba99128e0965a5416ebd41fed112a8
jkarlin929 <osida11572@gmail.com>
2018-01-12 16:29:43 -0500

    added another table to database, null value in base table

---
+++ db/migrations/migration-1515704663402.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS moonphasedata (
+  moonphasedata_id BIGSERIAL PRIMARY KEY NOT NULL,
+  moonrise VARCHAR(255),
+  moonset VARCHAR(255),
+  closestphase VARCHAR(255),
+  closestphasedate VARCHAR(255),
+  closestphasetime VARCHAR(255),
+  fracillum VARCHAR(255),
+  currentphase VARCHAR(255)
+);
+
+ALTER TABLE moonphase ADD COLUMN moonphasedata_id INTEGER;
